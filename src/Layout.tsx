dfb38145e45f6d7cda438be07dd84b7c75b24d19
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-28 17:43:08 +0900

    Feat: Have layout show head bar

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,9 +4,6 @@ function Layout() {
     return (
         <div>
             <h1>Hanaro Album</h1>
-            <input type="text" placeholder="User ID..." />
-            <button type="button">Sign In</button>
-            <div id="resultText"></div>
             <Outlet/>
         </div>
     );
