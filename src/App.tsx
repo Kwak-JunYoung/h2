d0c30c65d9e51464d9ed0158a91c175b857eb66e
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-28 16:20:04 +0900

    Feat: Addition of Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element='<Home />' />
+      </Routes>
     </div>
   );
 }
