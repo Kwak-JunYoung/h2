59da45c8c14e1b8530e12055a1bcb2bbfab0319e
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-28 16:55:43 +0900

    Feat: Addition of albumlist route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Route, Routes } from 'react-router-dom';
-
+import Layout from './Layout';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element='<Layout />' />
+        <Route path='/' element={<Layout />} />
+        <Route path='/albumList' element={<Layout />} />
       </Routes>
     </div>
   );
