8f9be2db54d47bdb967480c62ed3277ae1880ec0
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-28 17:38:44 +0900

    Feat: Implementation of home

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import AlbumList from './components/albumList';
 import { AppProvider } from './components/Context';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AppProvider>
         <Routes>
           <Route path='/' element={<Layout />} >
+            <Route index element={<Home />} ></Route>
             <Route path='/albumList' element={<AlbumList />} ></Route>
             <Route path='/albumList/:id' element={<AlbumList />} ></Route>
           </Route>
---
+++ src/components/albumList.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+function AlbumList() {
+    const baseUrl = "https://jsonplaceholder.typicode.com/albums/";
+
+    const controller = new AbortController();
+
+
+    useEffect(() => {
+        axios.get(baseUrl, { signal: controller.signal }).then((response) => {
+            console.log(response.data);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, []);
+
+    alert("CALLED")
+    return (
+        <div>
+            <h1>Album List</h1>
+        </div>
+    );
+}
+
+export default AlbumList;
