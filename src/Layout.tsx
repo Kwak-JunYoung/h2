4d3366e0cefc073760c648a396d2d0c18f078d34
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-28 21:50:03 +0900

    Feat: Addition of dynamic text that changes when logged in

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,21 @@
+import React, { useContext, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { AppContext } from "./components/Context";
 
 function Layout() {
+    let context = useContext(AppContext);
+
+    useEffect(() => {
+        console.log(context);
+    }, [context]); // 상태 변경 시마다 useEffect 실행
+
+    console.log(context.state);
     return (
         <div>
             <h1>Hanaro Album</h1>
+            {
+                context.state.isLogon ? <p>안녕하세요 {context.state.username}님</p> : <p>Login Please</p>
+            }
             <Outlet/>
         </div>
     );
