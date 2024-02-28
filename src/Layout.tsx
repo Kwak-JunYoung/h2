d3183af4a13f4d6723328249222f6752d889a4f7
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-28 23:01:35 +0900

    Fix: logout Feature

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,21 +1,29 @@
 import React, { useContext, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { AppContext } from "./components/Context";
 
 function Layout() {
     let context = useContext(AppContext);
 
+    let navigate = useNavigate();
+
     useEffect(() => {
         console.log(context);
     }, [context]); // 상태 변경 시마다 useEffect 실행
 
+    const logOut = () => {
+        context.dispatch({ type: "LOGOUT", value: { userid: "", username: "", isLogon: false } });
+        navigate("/");
+    }
+
     console.log(context.state);
     return (
         <div>
-            <h1>Hanaro Album</h1>
+            <h1 style={{"display": "inline"}}>Hanaro Album</h1>
             {
-                context.state.isLogon ? <p>안녕하세요 {context.state.username}님</p> : <p>Login Please</p>
+                context.state.isLogon ? <p>{context.state.userid} {context.state.username}</p> : <p>Login Please</p>
             }
+            <button onClick={logOut}>Logout</button>
             <Outlet/>
         </div>
     );
