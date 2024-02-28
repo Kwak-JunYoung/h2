1473224356c9d31832cbecc8b7ab9fa624c79279
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-29 00:00:46 +0900

    Feat: Implementation of back feature

--- src/components/pictureList.tsx
+++ src/components/pictureList.tsx
@@ -1,24 +1,43 @@
-import { useLocation, useParams, useSearchParams } from "react-router-dom";
+import { useLocation, useParams, useSearchParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PhotoType } from "./types/Type";
+import { AppContext, getStateFromLocalStorage } from "./Context";
 
 function PictureList() {
     const location = useLocation();
     const albumId = location.state.id;
-
     const [thumbnails, setThumbnails] = useState<PhotoType[]>([]);
+    const [albumTitle, setAlbumTitle] = useState<string>('');
 
-    axios.get(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`).then((response) => {
-        setThumbnails(response.data);
-    }
-    ).catch((error) => {
-        console.log(error);
-    })
+    let context = useContext(AppContext); 
+    
+    const navigate = useNavigate();
+    // navigate("/album", { state: userId }
+    useEffect(() => {
+        const controller = new AbortController(); //객체가 여기서 만들어져야 한다 
+
+        context.state = getStateFromLocalStorage("appState");//로그온한 아이디가져오고 다른 정보 불러온다 
+
+        axios.get(`https://jsonplaceholder.typicode.com//photos?albumId=${albumId}`).then((response) => {
+            setAlbumTitle(location.state.title);
+            setThumbnails(response.data);
+        }
+        ).catch((error) => {
+            console.log(error);
+        })
+        return () => {
+            console.log("마지막 정리작업을 하고 나간다 ");
+            console.log(location)
+            controller.abort(); //메모리 누스 해제, 백그라운드에서 작동중인 
+            //axios등을 멈추게 할 수 있다  
+        }
+    }, []);
+
 
     return (
         <div>
-            <p>picture Page</p>
+            <p>{albumTitle}</p>
             <ul>
                 {
                     thumbnails.map((item: PhotoType, key: number) => {
@@ -26,6 +45,7 @@ function PictureList() {
                     })
                 }
             </ul>
+            <button onClick={() => { navigate("/album",  { state: location.state.userId }) }}>돌아가기</button>
         </div>
     )
 }
