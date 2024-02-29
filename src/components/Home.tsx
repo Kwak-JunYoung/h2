75f3f9ba8b33679228bc4ce741f666f3d48bce84
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-29 11:35:43 +0900

    Chore: Refactoring of home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,13 +4,12 @@ import axios from 'axios';
 import { AppContext } from './Context';
 
 function Home() {
-    const [userId, setUserId] = useState<string>('');
-    const [msg, setMsg] = useState('');
-
-    const [resTxt, setResTxt] = useState('');
 
     let context = useContext(AppContext);
     let navigate = useNavigate();
+
+    const [userId, setUserId] = useState<string>('');
+    const [resTxt, setResTxt] = useState('');
 
     let onChange = (e: any) => {
         setUserId(e.target.value);
@@ -21,30 +20,22 @@ function Home() {
         // 위 문구를 아래 condition이 match하지 않으면 출력
         const cond = parseInt(userId) >= 1 && parseInt(userId) <= 10;
         let url = "https://jsonplaceholder.typicode.com/users/" + userId;
-        
+
         if (cond) {
             axios.get(url).then((response) => {
                 const userData = response.data;
                 let userName = userData.username;
 
                 context.dispatch({ type: "LOGON", value: { userid: userId, username: userName, isLogon: true } });
-                
+
                 setTimeout(() => { navigate("/album", { state: userId }) }, 1000);  //페이지 이동 
-                setMsg("");
             }).catch((error) => {
                 alert("로그인에 실패했습니다.");
-                setMsg("Login Fail");
             });
         }
         else {
             setResTxt("User ID는 1~10번만 가능합니다.");
-            alert('User ID는 1~10번만 가능합니다.')
-            setMsg("Login Fail");
         }
-    }
-
-    const logOut = () => {
-        context.dispatch({ type: "LOGOUT", value: { userid: "", username: "", isLogon: false } });
     }
 
     return (
@@ -52,7 +43,6 @@ function Home() {
             <input type="text" name="userId" id="userId" onChange={onChange} value={userId} placeholder="User ID..." />
             <button type="button" onClick={logIn}>Sign In</button>
             <div id="resultText" style={{ "color": "red" }}>{resTxt}</div>
-            {/* <Outlet/> */}
         </div>
     )
 
