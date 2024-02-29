de0e4567074bf8b2a01154c77ded434f6f4eb169
Kwak-JunYoung <klop9090@g.skku.edu>
2024-02-29 11:28:24 +0900

    Chore: Refactoring of album list

--- src/components/albumList.tsx
+++ src/components/albumList.tsx
@@ -1,10 +1,8 @@
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from "react";
 import { ItemType } from "./types/Type";
 import { useLocation, useNavigate } from "react-router-dom";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AppContext, getStateFromLocalStorage } from "./Context";
 
 const itemData: ItemType[] = [
     { userId: 2, id: 1, title: "title1" },
@@ -16,21 +14,17 @@ function AlbumList() {
 
     const location = useLocation();
     let navigate = useNavigate();
-    
+
     const [items, setItems] = useState<ItemType[]>(itemData);
-    const [item, setItem] = useState<ItemType>({ userId: -1, id: -1, title: "" });
+    const [item, setItem] = useState<ItemType>(itemData[0]);
 
     let uId = location.state;
-    
-    let context = useContext(AppContext);
 
     useEffect(() => {
         const controller = new AbortController();
-        context.state = getStateFromLocalStorage("appState");//로그온한 아이디가져오고 다른 정보 불러온다 
 
         axios.get(baseUrl, { signal: controller.signal }).then((response) => {
             const filtered = response.data.filter((item: ItemType) => item.userId == uId);
-
             setItems([...filtered]);
         }).catch((error) => {
             console.log(error);
@@ -38,12 +32,10 @@ function AlbumList() {
         return () => {
             controller.abort();
         }
-    }, []);
+    }, [uId]);
 
     const itemClick = (item: ItemType) => {
-        // 새로운 컴포넌트 만들어서 그쪽으로 이동?
-        setItem(item); // ...item으로 한다?
-        // navigate ("라우터에서 정한 url", {selected item})
+        setItem(item);
     }
 
     const btnClick = () => {
@@ -52,13 +44,21 @@ function AlbumList() {
 
     return (
         <div className="container mt-3" style={{ "marginTop": "30px" }}>
-            <div><h1 style={{display: "inline"}}>Album List</h1> <button type='button' className='button btn-primary' onClick={btnClick}>앨범 상세보기</button></div>
-            
+            <div>
+                <h1 style={{ display: "inline" }}>Album List</h1>
+                <button type='button' className='button btn-primary' onClick={btnClick}>앨범 상세보기</button>
+            </div>
+
             <ol className='list-group list-group-flush'>
                 {
                     items.map((i: ItemType, key: number) => {
-                        return <li className='list-group-item' key={i.id} onClick={() => { itemClick(i); }}
-                            style={i.id === item.id ? { "backgroundColor": "gray" } : { "backgroundColor": "white" }} >{key+1}. {i.title}</li>
+                        return <li
+                            className='list-group-item'
+                            key={i.id}
+                            onClick={() => { itemClick(i); }}
+                            style={i.id === item.id ? { "backgroundColor": "gray" } : { "backgroundColor": "white" }} >
+                            {key + 1}. {i.title}
+                        </li>
                     })
                 }
             </ol>
